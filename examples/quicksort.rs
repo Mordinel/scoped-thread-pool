4b90f82feaa8d078ec860b1c4be362ebd9421a2f
Stephane Neveu <stephane.neveu@ssi.gouv.fr>
2022-11-28 10:55:49 +0100

    Quickstart:   - Update rng.gen_range syntax ;   - Cargo format.

--- examples/quicksort.rs
+++ examples/quicksort.rs
@@ -1,6 +1,6 @@
-extern crate scoped_thread_pool;
 extern crate itertools;
 extern crate rand;
+extern crate scoped_thread_pool;
 
 use rand::Rng;
 
@@ -16,7 +16,7 @@ fn do_quicksort<'a, T: Send + Sync + Ord>(scope: &Scope<'a>, data: &'a mut [T]) 
             // Choose a random pivot.
             let mut rng = rand::thread_rng();
             let len = data.len();
-            let pivot_index = rng.gen_range(0, len); // Choose a random pivot
+            let pivot_index = rng.gen_range(0..len); // Choose a random pivot
 
             // Swap the pivot to the end.
             data.swap(pivot_index, len - 1);
@@ -47,24 +47,27 @@ pub fn main() {
     let pool = Pool::new(8);
 
     for _ in 0..10 {
-        let n = rng.gen_range(1, 10000000);
+        let n = rng.gen_range(1..10000000);
 
         println!("Generating {} random elements!", n);
         let mut elements = (0..n).map(|_| ::rand::random::<u64>()).collect::<Vec<_>>();
-
 
         println!("Sorting elements!");
         quicksort(&pool, &mut elements);
         println!("Finished sorting elements!");
 
-
         println!("Verifying the elements are sorted.");
         for (current, next) in elements.iter().zip(elements[1..].iter()) {
-            assert!(next >= current, "{:?} < {:?}! elements: {:?}", next, current, elements);
+            assert!(
+                next >= current,
+                "{:?} < {:?}! elements: {:?}",
+                next,
+                current,
+                elements
+            );
         }
     }
 
     pool.shutdown();
     println!("Success.");
 }
-
